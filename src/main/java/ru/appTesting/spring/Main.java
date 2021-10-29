a8bc1c0b9ded34ea84a3ae239a0f9c54e55d7c4a
ilyazv <zeshot@yandex.ru>
2021-10-30 00:48:52 +0300

    Up to spring boot; Update README.MD;

--- src/main/java/ru/appTesting/spring/Main.java
+++ src/main/java/ru/appTesting/spring/Main.java
@@ -1,18 +1,15 @@
 package ru.appTesting.spring;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import ru.appTesting.spring.config.Config;
 import ru.appTesting.spring.service.RunService;
 
 @EnableAspectJAutoProxy
-@Configuration
-@ComponentScan
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        var context = new AnnotationConfigApplicationContext(Main.class);
+        var context = SpringApplication.run(Main.class);
         RunService runService = context.getBean(RunService.class);
         runService.runTest();
     }
